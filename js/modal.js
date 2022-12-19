9803a73b0e4463b2e507616a7d1d60f8b0e729c0
LogicTypist <1265pcs@gmail.com>
2022-12-20 02:00:20 +0900

    아이콘 출처 추가 및 행성 설명 비활성화 : 저작권 자료 재검토

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,9 @@
 
 let docs = [];
 export async function loadDoc(body){
-    docs[body.nameEn] = await fetchHtmlAsText('./bodyDocs/detail/' + body.nameEn + '.html');
+    //docs[body.nameEn] = await fetchHtmlAsText('./bodyDocs/detail/' + body.nameEn + '.html');
+
+    docs[body.nameEn] = "";     //상세정보 비활성화
 }
 
 async function fetchHtmlAsText(url) {
@@ -10,10 +12,10 @@ async function fetchHtmlAsText(url) {
 
 export function importPage(target) {
     document.getElementById('modal_header').innerHTML = "<p id = 'bodyNameKor'>" + target.nameKor +"</p><p id = 'bodyNameEn'>" + target.nameEn + "</p><div id = 'show_details'> 상세정보 </div>";
-    document.getElementById('body_details').innerHTML = docs[target.nameEn];
+    //document.getElementById('body_details').innerHTML = docs[target.nameEn];      //상세정보 비활성화
 }
 
 export function resetPage(){
     document.getElementById('modal_header').innerHTML = "<p class='init_message'> 정보를 보려면 천체를 선택하세요. </p>";
-    document.getElementById('body_details').innerHTML = "";
+    //document.getElementById('body_details').innerHTML = "";      //상세정보 비활성화
 }
